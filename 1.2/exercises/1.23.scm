994155cf5b9cb1405e3ac8d667b10624981b2382
kame <kameyasu119@gmail.com>
2016-03-19 13:53:28 +0900

    1.23

---
+++ 1.2/exercises/1.23.scm
@@ -0,0 +1,45 @@
+(define (next n)
+  (if (= n 2)
+    3
+    (+ n 2)))
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+(define (divides? a b) (= (remainder b a) 0))
+(define (runtime)
+      (use srfi-11)
+            (let-values (((a b) (sys-gettimeofday)))
+                                                        (+ (* a 1000000) b)))
+(define (prime? n)
+      (= n (smallest-divisor n)))
+(define (timed-prime-test n)
+    (newline)
+      (display n)
+        (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+    (if (prime? n)
+          (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+    (display "***")
+      (display elapsed-time))
+
+
+(define (odd? n)
+    (= (remainder n 2) 1))
+(define (search-for-primes from n)
+    (cond ((odd? from) (start-search from n 1))
+                  (else (start-search (+ from 1) n 1))))
+(define (start-search from n count )
+    (cond ((< n count) (display "end"))
+                  (else (timed-prime-test from)
+                                      (start-search (+ from 1) n (+ count 1)))))
+
+(print (search-for-primes 1000 100))
+
+(print (search-for-primes 10000 100))
+(print (search-for-primes 100000 100))
+
+;1.22と比べて、二倍にはなっていない。
+;今回減らした2の倍数の時はsmallest-divisorは速攻終わるからではないかと
